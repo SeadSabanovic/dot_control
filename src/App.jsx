3319b020f1cc6c5199b7d748583cdf9317df4b69
Sead Sabanovic <s.sabanovic@agentur-loop.com>
2024-01-28 19:02:51 +0100

    Add Text Font - Newzald

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
   return (
     <>
       <Rating />
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, ad a inventore provident ex autem odit cupiditate vitae quas ab suscipit placeat eveniet sint voluptates incidunt! Eaque eos quis velit.
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur,
+        odio.
+      </p>
     </>
   );
 }
