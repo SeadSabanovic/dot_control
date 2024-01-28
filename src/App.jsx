197973db041346f1e6dd43960411f0e0d6d1184c
Sead Sabanovic <s.sabanovic@agentur-loop.com>
2024-01-28 18:27:24 +0100

    Add Title Font - Galano Grotesque

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.scss'
-import Rating from './components/Rating'
+import "./App.scss";
+import Rating from "./components/Rating";
 
 function App() {
-
   return (
     <>
       <Rating />
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, ad a inventore provident ex autem odit cupiditate vitae quas ab suscipit placeat eveniet sint voluptates incidunt! Eaque eos quis velit.
     </>
-  )
+  );
 }
 
-export default App
+export default App;
